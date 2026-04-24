714da7f4673ca8aedbb0509004c30556ec50f7af
ABDULKERIM ADEM <abdukilade456@gmail.com>
2026-04-24 11:02:04 +0300

    added the push notification but in order to make ot automatic when sending that the notification gets trigered we require the cloude functions of firebase but that is on the paid plan so i couldn't add it, sorry.

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,10 +1,30 @@
 import 'package:chat_app/widget/chat_messages.dart';
 import 'package:chat_app/widget/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  void setupPushNotifications() async {
+    final fcm = FirebaseMessaging.instance;
+    await fcm.requestPermission();
+    fcm.subscribeToTopic('chat');
+  }
+
+  @override
+  void initState() {
+    // the reason why we can't make initState async is because it is a lifecycle method and it needs to be called synchronously when the widget is created. If we make it async, it would return a Future and the widget would not be able to properly initialize itself. Instead, we can call an async function from within initState to perform any necessary asynchronous operations, such as requesting permissions or fetching data.
+    super.initState();
+
+    setupPushNotifications();
+  }
 
   @override
   Widget build(BuildContext context) {
